04a55423a44b8c0f2bcd582fadef8429d638f967
Fenrir Flour <fenrirfl@student.21-school.ru>
2025-06-12 14:10:56 +0300

    Ваше сообщение коммита

--- web/urls.py
+++ web/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('input_data/', views.input_data, name='input_data'),
-] 
+    path('result/', views.result, name='result'),  # Путь к странице с результатом
+    path('register/', views.register, name='register'),  # Путь к странице регистрации
+    path('login/', views.login, name='login'),  # Путь к странице входа
+    path('profile/', views.profile, name='profile'),  # Путь к странице профиля
+]
