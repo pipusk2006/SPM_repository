a3e86162ad08a70418cbae3b413225cdf5045593
Fenrir Flour <fenrirfl@student.21-school.ru>
2025-06-12 14:22:34 +0300

    Ваше сообщение коммита

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,8 +5,11 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('profile/', views.profile, name='profile'),
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('input_data/', views.input_data, name='input_data'),
+    path('result/', views.result, name='result'),  # Путь к странице с результатом
+    path('register/', views.register, name='register'),  # Путь к странице регистрации
+    path('login/', views.login, name='login'),  # Путь к странице входа
+    path('profile/', views.profile, name='profile'),  # Путь к странице профиля
 ]
